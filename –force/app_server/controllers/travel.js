8139f70d7c00668c4a3a01b448e5d8a683601bf7
Idriss Kacou <idrisskacou@outlook.com>
2023-09-16 16:35:25 -0700

    convert the travel data from static data to dynamic JSON

--- –force/app_server/controllers/travel.js
+++ –force/app_server/controllers/travel.js
@@ -1,5 +1,11 @@
+const fs = require('fs');
+const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
+/*
+Get the traveler view
+*/
 const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways' });
+    res.render('travel', { title: 'Travlr Getaways',trips });
 };
 
 module.exports = {
