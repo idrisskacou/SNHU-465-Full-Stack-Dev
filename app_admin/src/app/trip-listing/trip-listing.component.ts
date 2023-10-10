20941ff0e81c399e102cfe1eda1f297790c10ee6
Idriss Kacou <idrisskacou@outlook.com>
2023-10-09 19:33:49 -0700

    Still work start from the beginning

---
+++ app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trip-listing',
+  templateUrl: './trip-listing.component.html',
+  styleUrls: ['./trip-listing.component.css']
+})
+export class TripListingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
