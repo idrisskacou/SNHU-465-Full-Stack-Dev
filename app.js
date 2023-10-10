4e4258d41ae1bbfe96e26e1359580bc988ea6e54
Idriss Kacou <idrisskacou@outlook.com>
2023-10-09 20:26:29 -0700

    Still working install trip service add

--- app.js
+++ app.js
@@ -29,6 +29,13 @@ hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'));
 
 app.set('view engine', 'hbs');
 
+// Allow CORS
+app.use('api', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Headers', 'Orgin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods','GET, POST, PUT, DELETE');
+  next();
+});
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
